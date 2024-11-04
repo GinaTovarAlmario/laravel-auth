ce6e75c991650939672f4c51881009a1a9d0c60b
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-04 18:00:43 +0100

    added redirect to store

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -29,7 +29,7 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return redirect()->route('admin.projects.index');
     }
 
     /**
