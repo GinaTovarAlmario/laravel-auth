220b74fe8b30cc6884475c72318a50c7c29e1e50
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-04 17:43:35 +0100

    added route to index admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/projects',[AdminProjectController::class,'index'])->name('admin.projects.index');
