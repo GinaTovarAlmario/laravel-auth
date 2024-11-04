687e55b27a5846f52976d9e3c84dfbe877bc7428
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-04 18:04:31 +0100

    added model to index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = [];
+        $projects = Project::all();
         return view('admin.projects.index', compact('projects'));
     }
 
