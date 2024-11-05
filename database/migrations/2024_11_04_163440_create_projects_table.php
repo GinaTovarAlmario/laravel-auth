a8f19cd071ee6d8f4ad63d506356849f5e0ef3a2
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-05 17:35:16 +0100

    added data and migration

--- database/migrations/2024_11_04_163440_create_projects_table.php
+++ database/migrations/2024_11_04_163440_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title',80)->nullable(false);
+            $table->string('author',50)->nullable(false);
+            $table->date('date');
+            $table->text('description');
             $table->timestamps();
         });
     }
