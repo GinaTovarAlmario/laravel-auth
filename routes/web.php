d11ac6518ab5931cd812c4ba1fc0832a3f8ba809
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-05 22:04:40 +0100

    organized order routes

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
     Route::get('/projects', [AdminProjectController::class, 'index'])->name('projects.index');
     Route::post('/projects', [AdminProjectController::class, 'store'])->name('projects.store');
-    Route::get('/projects/create', [AdminProjectController::class, 'create'])->name('projects.create');
-    Route::get('/projects/{project}', [AdminProjectController::class, 'show'])->name('projects.show');
 
-    Route::put("/albums/{project}", [AdminProjectController::class, "update"])->name("projects.update");
+    Route::get('/projects/create', [AdminProjectController::class, 'create'])->name('projects.create');
     Route::delete("/albums/{project}", [AdminProjectController::class, "destroy"])->name("projects.delete");
+    Route::put("/albums/{project}", [AdminProjectController::class, "update"])->name("projects.update");
+    Route::get('/projects/{project}', [AdminProjectController::class, 'show'])->name('projects.show');
     Route::get("/albums/{project}/edit", [AdminProjectController::class, "edit"])->name("projects.edit");
 
 
